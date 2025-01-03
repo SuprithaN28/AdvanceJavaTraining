d4003be8b01acf40bf114111eb43ac1f8adda141
SuprithaN28 <109121202+SuprithaN28@users.noreply.github.com>
2025-01-03 17:03:25 +0530

    eureka server code

--- src/main/java/com/training/advancedJava/AdvancedJavaApplication.java
+++ src/main/java/com/training/advancedJava/AdvancedJavaApplication.java
@@ -7,11 +7,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.training.advancedJava")
 @EntityScan(basePackages = "com.training.advancedJava")
+@EnableEurekaServer
 public class AdvancedJavaApplication implements CommandLineRunner {
 
     @Autowired
